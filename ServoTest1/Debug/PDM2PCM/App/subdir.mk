e827737fd6ca21f515ef162ff77bb0cb2f7654b2
sydorbogdan <b.sydor@ucu.edu.ua>
2020-11-02 20:31:58 +0200

    uploaded servoSG90 test projects

---
+++ ServoTest1/Debug/PDM2PCM/App/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../PDM2PCM/App/pdm2pcm.c 
+
+OBJS += \
+./PDM2PCM/App/pdm2pcm.o 
+
+C_DEPS += \
+./PDM2PCM/App/pdm2pcm.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+PDM2PCM/App/pdm2pcm.o: ../PDM2PCM/App/pdm2pcm.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F411xE -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -I../PDM2PCM/App -I../Middlewares/ST/STM32_Audio/Addons/PDM/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"PDM2PCM/App/pdm2pcm.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
